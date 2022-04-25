7d1fc2b52737c7375f6a2b4a781d569ebda817e1
Александр <zbc@mail.ru>
2022-04-25 19:11:48 +0300

    add base webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
