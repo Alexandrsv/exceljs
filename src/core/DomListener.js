3d10693e46f0a5a03db3dfc72d6a83e7fb56ee9e
Александр <zbc@mail.ru>
2022-04-27 18:26:46 +0300

    add layout formation

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,1 +1,8 @@
-export class DomListener {}
+export class DomListener {
+  constructor($root) {
+    if (!$root) {
+      throw new Error("$root is required for DomListener");
+    }
+    this.$root = $root;
+  }
+}
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,6 +1,7 @@
 import { DomListener } from "core/DomListener";
 
 export class ExcelComponent extends DomListener {
+  static className2 = `excel__${this.constructor.name.toLowerCase()}`;
   // Возвращает шаблон компонента
   toHTML() {
     return "";
